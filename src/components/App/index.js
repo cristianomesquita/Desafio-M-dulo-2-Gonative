99c4e8dd11070a81a542bcecb7b59cff11a5854c
Cristiano Mesquita <cristianomesquita@Cristianos-MacBook-Air.local>
2018-08-22 19:30:43 -0300

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+import 'config/ReactotronConfig'
+import 'config/DevToolsConfig'
+
+const App = () => <Text>APP</Text>
+
+export default App
