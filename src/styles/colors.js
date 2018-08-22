4d37f12fcdd9708856827508532f3b10a33c9661
Cristiano Mesquita <cristianomesquita@Cristianos-MacBook-Air.local>
2018-08-22 19:52:07 -0300

    add: base styles

---
+++ src/styles/colors.js
@@ -0,0 +1,13 @@
+export default {
+  white: '#FFF',
+  light: '#EEE',
+  regular: '#DDD',
+  dark: '#666',
+  darker: '#333',
+
+  secundary: '#999',
+
+  transparent: 'transparent',
+  darkTransparent: 'rgba(0,0,0,0.6)',
+  whiteTransparent: 'rgba(255,255,255,0.3)'
+}
