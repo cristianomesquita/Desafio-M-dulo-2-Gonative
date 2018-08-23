8ff3120424e7ad26de53fa8ec0959af062b5f0c4
Cristiano Mesquita <cristianomesquita@Cristianos-MacBook-Air.local>
2018-08-23 11:42:18 -0300

    update: pages/repositories

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,20 +1,43 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View, TextInput, TouchableOpacity, FlatList } from 'react-native'
 import { NavigationActions } from 'react-navigation'
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+import api from 'services/api'
+
+import styles from './styles'
 
 class Repositories extends React.Component {
   static navigationOptions = {
     title: 'GitIssues'
   }
 
-  componentDidMount() {
-    console.tron.log(this.props)
-    const navigation = NavigationActions.navigate({ routeName: 'Issues' })
-    this.props.navigation.dispatch(navigation)
+  async componentDidMount() {
+    try {
+      const response = await api.get(`/repos/react-community/react-navigation`)
+      console.tron.log(response)
+    } catch (error) {
+      console.tron.log(error)
+    }
   }
 
   render() {
-    return <Text>Repositories</Text>
+    return (
+      <View style={styles.container}>
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicionar novo repositório"
+            autoCapitalize="none"
+            autoCorrect={false}
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+          <TouchableOpacity style={styles.button}>
+            <Icon name="plus" size={20} />
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 }
 
